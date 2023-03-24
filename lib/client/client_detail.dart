118534e019cbf95455cc260668b982a226a3d10f
Alexander Vargas <cavale610@gmail.com>
2023-03-24 03:48:22 -0500

    Four

--- lib/client/client_detail.dart
+++ lib/client/client_detail.dart
@@ -11,92 +11,96 @@ class DetailsClient extends StatelessWidget {
       appBar: AppBar(
         title: Text("Detalles del cliente"),
       ),
-      body: Container(
-        height: 400,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(25)),
-            border: Border.all(color: Colors.black)),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 100,
-                  height: 100,
-                  child: Image.network(
-                      "https://icons.iconarchive.com/icons/icons8/windows-8/512/Users-Find-User-icon.png"),
-                ),
-                Text(
-                  "Cliente #${client.id}",
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text("${client.name} ${client.lastName}",
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Container(
+          height: 360,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+              border: Border.all(color: Colors.black)),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 100,
+                    child: Image.network(
+                        "https://icons.iconarchive.com/icons/icons8/windows-8/512/Users-Find-User-icon.png"),
+                  ),
+                  Text(
+                    "Cliente #${client.id}",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text("${client.name} ${client.lastName}",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold)),
+                  Text("Correo electronico: ${client.email}",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                  Text("Telefono: ${client.phone}",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                  Text("Direccion:  ${client.address}",
+                      style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold)),
+                  Text(
+                    client.status == 1 ? "ACTIVO" : "INACTIVO",
                     style: const TextStyle(
                         color: Colors.black,
                         fontSize: 25,
-                        fontWeight: FontWeight.bold)),
-                Text("Correo electronico: ${client.email}",
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold)),
-                Text("Telefono: ${client.phone}",
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold)),
-                Text("Direccion:  ${client.address}",
-                    style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold)),
-                Text(
-                  client.status == 1 ? "ACTIVO" : "INACTIVO",
-                  style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold),
-                ),
-                Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStateProperty.resolveWith(
+                                        (states) => Colors.red)),
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Cancelando')),
+                              );
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancelar',
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
                               backgroundColor:
                                   MaterialStateProperty.resolveWith(
-                                      (states) => Colors.red)),
-                          onPressed: () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Cancelando')),
-                            );
-                            Navigator.pop(context);
-                          },
-                          child: const Text('Cancelar',
-                              style: TextStyle(color: Colors.white)),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.resolveWith(
-                                (states) => Colors.black),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            'Editar',
-                            style: TextStyle(color: Colors.white),
+                                      (states) => Colors.black),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'Editar',
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
